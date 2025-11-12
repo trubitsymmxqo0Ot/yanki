ae8310a9ef5dc9ca47aa3d78737dc54d34d6349d
trubitsymmxqo0Ot <maxtrubitsyn3@mail.ru>
2025-11-12 17:59:39 +0300

    Подключил bundle analyzer к проекту

--- config/webpack/buildPlugins.ts
+++ config/webpack/buildPlugins.ts
@@ -1,23 +1,35 @@
 import webpack, { Configuration } from "webpack";
-import HtmlWebpackPlugin from 'html-webpack-plugin';
+import HtmlWebpackPlugin from "html-webpack-plugin";
 import { BuildPaths } from "./types/types";
-import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import { BundleAnalyzerPlugin } from "webpack-bundle-analyzer";
 
-export function buildPlugins(isDev: boolean, paths: BuildPaths): Configuration['plugins'] {
-    const plugins = [];
-    plugins.push(new HtmlWebpackPlugin({
-        filename: isDev ? "index.html" : "[name].[contenthash].html",
-        template: paths.html,
-        favicon: paths.favicon,
-        minify: isDev ? false : true,  
-    }))
-    if(isDev) {
-        plugins.push(new webpack.ProgressPlugin());
-    } else {
-        plugins.push(new MiniCssExtractPlugin({
-            filename: "[name].[contenthash].css"
-            
-        }))
-    }
-    return plugins;
+export function buildPlugins(
+  isDev: boolean,
+  paths: BuildPaths
+): Configuration["plugins"] {
+  const plugins = [];
+  plugins.push(
+    new HtmlWebpackPlugin({
+      filename: isDev ? "index.html" : "[name].[contenthash].html",
+      template: paths.html,
+      favicon: paths.favicon,
+      minify: isDev ? false : true,
+    })
+  );
+  if (isDev) {
+    plugins.push(new webpack.ProgressPlugin());
+    plugins.push(new BundleAnalyzerPlugin({
+        analyzerMode: "server",
+        analyzerPort: 3009,
+        openAnalyzer: true,
+    }));
+  } else {
+    plugins.push(
+      new MiniCssExtractPlugin({
+        filename: "[name].[contenthash].css",
+      })
+    );
+  }
+  return plugins;
 }
